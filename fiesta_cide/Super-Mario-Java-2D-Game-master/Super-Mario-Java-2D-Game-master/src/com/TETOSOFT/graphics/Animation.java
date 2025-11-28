006cd183a2ffde74f024fbde75f8dd5731701443
NicoRuedaA <nico99rueda@gmail.com>
2025-11-28 20:28:29 +0100

    28/11

--- fiesta_cide/Super-Mario-Java-2D-Game-master/Super-Mario-Java-2D-Game-master/src/com/TETOSOFT/graphics/Animation.java
+++ fiesta_cide/Super-Mario-Java-2D-Game-master/Super-Mario-Java-2D-Game-master/src/com/TETOSOFT/graphics/Animation.java
@@ -3,94 +3,69 @@ package com.TETOSOFT.graphics;
 import java.awt.Image;
 import java.util.ArrayList;
 
-public class Animation 
-{
+public class Animation {
 
     private ArrayList frames;
     private int currFrameIndex;
     private long animTime;
     private long totalDuration;
 
-
-    public Animation() 
-    {
+    public Animation() {
         this(new ArrayList(), 0);
     }
 
-
-    private Animation(ArrayList frames, long totalDuration) 
-    {
+    private Animation(ArrayList frames, long totalDuration) {
         this.frames = frames;
         this.totalDuration = totalDuration;
         start();
     }
 
-
-    public Object clone() 
-    {
+    public Object clone() {
         return new Animation(frames, totalDuration);
     }
 
-
-    public synchronized void addFrame(Image image, long duration)
-    {
+    public synchronized void addFrame(Image image, long duration) {
         totalDuration += duration;
         frames.add(new AnimFrame(image, totalDuration));
     }
 
-
-    public synchronized void start() 
-    {
+    public synchronized void start() {
         animTime = 0;
         currFrameIndex = 0;
     }
 
-
-    public synchronized void update(long elapsedTime) 
-    {
-        if (frames.size() > 1) 
-        {
+    public synchronized void update(long elapsedTime) {
+        if (frames.size() > 1) {
             animTime += elapsedTime;
 
-            if (animTime >= totalDuration) 
-            {
+            if (animTime >= totalDuration) {
                 animTime = animTime % totalDuration;
                 currFrameIndex = 0;
             }
 
-            while (animTime > getFrame(currFrameIndex).endTime) 
-            {
+            while (animTime > getFrame(currFrameIndex).endTime) {
                 currFrameIndex++;
             }
         }
     }
 
-
-    public synchronized Image getImage() 
-    {
-        if (frames.size() == 0) 
-        {
+    public synchronized Image getImage() {
+        if (frames.size() == 0) {
             return null;
-        }
-        else 
-        {
+        } else {
             return getFrame(currFrameIndex).image;
         }
     }
 
-
-    private AnimFrame getFrame(int i) 
-    {
-        return (AnimFrame)frames.get(i);
+    private AnimFrame getFrame(int i) {
+        return (AnimFrame) frames.get(i);
     }
 
-    private class AnimFrame 
-    {
+    private class AnimFrame {
         Image image;
         long endTime;
 
-        public AnimFrame(Image image, long endTime) 
-        {
+        public AnimFrame(Image image, long endTime) {
             this.image = image;
             this.endTime = endTime;
         }
